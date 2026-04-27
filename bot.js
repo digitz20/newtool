3805a91b8b2d30a12f555a63947bd00b2678955b
digitz20 <alaekekaebuka200@gmail.com>
2026-04-27 09:07:25 +0100

    kre

--- bot.js
+++ bot.js
@@ -527,7 +527,7 @@ async function sendEmail(to, lead, leadSenderName) {
   const mailOptions = {
     from: fromAddress,
     to: to,
-    subject: 'Hello from ' + leadSenderName + 'Treat as Urgent',
+    subject: 'Hello from ' + leadSenderName + ' treat as Urgent',
     html: htmlContent,
     replyTo: (lead && lead.sender && lead.sender.email) ? lead.sender.email : undefined,
   };
